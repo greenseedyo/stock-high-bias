cacd64fddd6903c06bd51504b0ba3629778d21ce
yo <turtleegg@gmail.com>
2018-09-07 00:52:37 +0800

    update crawler

--- crawler.php
+++ crawler.php
@@ -1,5 +1,6 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+date_default_timezone_set("Asia/Taipei");
 $loader = require __DIR__ . '/vendor/autoload.php';
 
 use Luyo\Stock\Nlog\HighBiasCrawler;
@@ -11,6 +12,27 @@ use PHPMailer\PHPMailer\Exception;
 
 class PickCommand
 {
+    private $latest_result_file;
+    private $history_dir;
+    private $date;
+
+    public function __construct(PickConfig $config)
+    {
+        $data_dir = $config->data_dir;
+        $this->date = $config->date ?: date('Ymd');
+        if (!is_dir($data_dir)) {
+            mkdir($data_dir, 0777, true);
+        }
+        $this->latest_result_file = "{$data_dir}/latest_result.json";
+        $this->history_dir = "{$data_dir}/history";
+        if (!file_exists($this->latest_result_file)) {
+            touch($this->latest_result_file);
+        }
+        if (!file_exists($this->history_dir)) {
+            mkdir($this->history_dir, 0777, true);
+        }
+    }
+
     public function setBiasFilePath($file_path)
     {
         $this->bias_file_path = $file_path;
@@ -61,7 +83,7 @@ class PickCommand
         $index_crawler = new IndexCrawler;
         $index_info_obj = $index_crawler->run();
         $latest_trade_day = $index_info_obj->date;
-        if (date('Ymd') !== $latest_trade_day) {
+        if ($this->date !== $latest_trade_day) {
             return false;
         }
         return true;
@@ -86,17 +108,51 @@ class PickCommand
             return true;
         }, ARRAY_FILTER_USE_BOTH);
         $i = 0;
+        $latest_result = $this->getLatestResult();
         foreach ($filtered_high_bias_stocks as $code => $bias) {
             $i ++;
             $name = $stock_list[$code];
-            $result[] = "({$i}) {$code} {$name}: {$bias}";
+            $line = "{$i}. {$code} {$name}: {$bias}";
+            if (isset($latest_result[$code])) {
+                $line .= ' (new)';
+            }
+            $result[] = $line;
         }
         print_r($result);
         $content = implode("\n", $result);
         $this->sendMessage($content);
+        $this->saveToLatestResult($filtered_high_bias_stocks);
+        $this->saveToHistory($filtered_high_bias_stocks);
     }
 
-    private function sendMessage($content)
+    private function getLatestResult(): array
+    {
+        $latest_result = json_decode(file_get_contents($this->latest_result_file), 1);
+        if (0 === strlen($latest_result)) {
+            echo "{$this->latest_result_file}: no contents, ignore.\n";
+            return array();
+        }
+        if (!is_array($latest_result)) {
+            echo "{$this->latest_result_file}: file contents are not in json format, ignore.\n";
+            return array();
+        }
+        return $latest_result;
+    }
+
+    private function saveToLatestResult(array $stock_codes): void
+    {
+        $contents = json_encode($stock_codes);
+        file_put_contents($this->latest_result_file, $contents);
+    }
+
+    private function saveToHistory(array $stock_codes): void
+    {
+        $file = "{$this->history_dir}/{$data}.json";
+        $contents = json_encode($stock_codes);
+        file_put_contents($file, $contents);
+    }
+
+    private function sendMessage($content): void
     {
         $subject = sprintf("%s 高乖離率", date('Y-m-d'));
         $mail = new PHPMailer(true);
@@ -111,11 +167,21 @@ class PickCommand
     }
 }
 
-$command = new PickCommand;
+
+class PickConfig
+{
+    public $data_dir = 'data';
+    public $date;
+}
+
+
+$config = new PickConfig;
+//$config->date = '20180906';
+$command = new PickCommand($config);
 if (!$command->checkDate()) {
     die('今天非交易日');
 }
-//$command->setBiasFilePath(__DIR__ . "/tests/nlog_bias.html");
-//$command->setCapitalReductionFilePath(__DIR__ . "/tests/capital_reduction.json");
-//$command->setStockListFilePath(__DIR__ . "/tests/stock_list.html");
+$command->setBiasFilePath(__DIR__ . "/tests/nlog_bias.html");
+$command->setCapitalReductionFilePath(__DIR__ . "/tests/capital_reduction.json");
+$command->setStockListFilePath(__DIR__ . "/tests/stock_list.html");
 $command->exec();
